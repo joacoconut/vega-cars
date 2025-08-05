f1001044ea1720d1004fdc293ac6246ed61f9500
Joaquin Vega <joaquinvega241200@gmail.com>
2025-08-05 03:48:27 -0300

    handler fixed

--- app/api/reservations/[id]/route.ts
+++ app/api/reservations/[id]/route.ts
@@ -1,13 +1,16 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function DELETE(
-  req: Request,
-  context: { params: { id: string } }
-) {
-  try {
-    const { id } = context.params;
+export async function DELETE(req: Request) {
+  // Extraer 'id' del pathname
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop();
 
+  if (!id) {
+    return new NextResponse("ID not provided", { status: 400 });
+  }
+
+  try {
     const deleted = await db.order.delete({
       where: { id },
     });
